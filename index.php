042c8f70081c281836d68f859e0e16a2737f0bc3
domrachev-andrey <andrey@domrachev.ru>
2022-11-05 21:22:08 +0300

    final at 05.11.22 21:21
    final at 05.11.22 21:21
    design was did. uploaded to hosting.

---
+++ index.php
@@ -0,0 +1,93 @@
+
+<html>
+<head>
+<meta http-equiv=Content-Type content=text/html; charset=windows-1251>
+<link href=http://mtw.ru/myvisit/mtw.css rel=stylesheet type=text/css>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Z60ZKXEM0W"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Z60ZKXEM0W');
+</script>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(90960295, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/90960295" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+<title>
+Домрачев Консалтинг
+</title>
+</head>
+<body background=http://mtw.ru/myvisit/marble.jpg>
+<table width=100% height=100% bgcolor=ECF3F9 background=http://mtw.ru/myvisit/marble.jpg>
+		<tr valign=top align=center>
+			<td><br><br><br><br><br><br><br><br><br><br><br>
+			<table border=0 cellspacing=0 cellpadding=0 width=38%>
+				<tr>
+					<td bgcolor=#dde4f4>
+				
+						    <table align=center border=0 cellspacing=1 cellpadding=4 width=100%>
+							<tr align=center>
+							    <td background=http://mtw.ru/myvisit/bak.gif class=text3 align=left>
+							    <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+							    <font color=#3c4b8b><b>Домрачев Консалтинг</b></font>
+							    <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+							    <font color=#808080>1С Франчайзи</font>
+							    <br>
+							    <br>
+							    </td>
+							</tr>
+<tr>
+<td bgcolor=white>
+<table align=center border=0 cellspacing=0 cellpadding=4 width=100%>
+	<tr>
+
+	<td align=right valign=top bgcolor=#FFFFFF>
+	<img src=http://mtw.ru/images/myindex/mail.gif>
+	</td>
+	<td valign=top bgcolor=#FFFFFF class=text3>
+	<font color=#3c4b8b>
+	<b>E-mail</b>
+	</font>
+	<br>
+	<a href=mailto:1c@domrachev.ru><u>1c@domrachev.ru</u></a>
+	</td>
+
+	<td align=right valign=top bgcolor=#FFFFFF>
+	<img src=http://mtw.ru/images/myindex/tel.gif>
+	</td>
+	<td valign=top bgcolor=#FFFFFF class=text3>
+	<font color=#3c4b8b>
+	<b>Телефон</b>
+	</font>
+	<br>
+	<font color=#808080>+7(499)793-69-71</font></a>
+	</td>
+</tr><tr>
+</tr>
+</table>
+</td>
+</tr>
+					</table>
+					</td>
+				</tr>
+			</table>
+			</td>
+			</tr>
+			</table>
+</body>	
+</html>
