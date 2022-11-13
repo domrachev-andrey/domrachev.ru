de3262c42de3ecb317b4ceca36657acdf3b1dc38
domrachev-andrey <andrey@domrachev.ru>
2022-11-13 04:46:49 +0300

    13.11.22 04:46
    13.11.22 04:46
    some php

---
+++ bottom.php
@@ -0,0 +1,23 @@
+	<footer class="main-footer">
+		<div class="container">
+			<section class="footer-company">
+				<p>ООО "Домрачев"<br>
+				   ОГРН 1217700584111<br>
+				   ИНН 7727478970
+				</p>	
+			</section>
+			<section class="footer-contacts">
+				<p>Наши контакты<br>
+				   <a href="tel:+7(499)793-69-71">+7(499)793-69-71</a><br>
+				   <a href="mailto:1c@domrachev.ru">1c@domrachev.ru</a>
+				</p>	
+			</section>
+			<section class="footer-social">
+				<p>Мы в социальных сетях<br>
+				   <a href="https://vk.com/domrachev_consulting">В контакте</a>
+				</p>	
+			</section>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,31 @@
+<?php
+	include 'top.php';
+?>
+	<main class="container">
+		<div class="index-slider">
+			<!-- <img src="https://dummyimage.com/600x400/aaa/fff" alt=""> -->
+		</div>
+		<div class="index-item">		
+			<div class="index-item-left">
+				<img src="img/logo1cfm.png" alt="">
+				<p>Программы 1С</p>
+			</div>
+			<div class="index-item-right">
+				<img src="img/fran1cp.png" alt="">
+				<p>Внедрение 1С</p>
+			</div>
+		</div>	
+		<div class="index-item">		
+			<div class="index-item-left">
+				<img src="img/1citsm.png" alt="">
+				<p>1С ИТС</p>
+			</div>
+			<div class="index-item-right">
+				<a href="1cfresh"><img src="img/1cfresh.png" alt=""></a>
+				<a href="1cfresh"><p>1С Фреш</p></a>
+			</div>
+		</div>
+	</main>
+<?php
+	include 'bottom.php';
+?>
